37e8f53994c83e6f29725b7521b80238c95ef5c9
Resford <resford1@gmail.com>
2023-11-12 22:17:26 -0700

    figured it fucking out

--- tutorial-dbt-dagster/jaffle_shop/jaffle_dagster/jaffle_dagster/assets.py
+++ tutorial-dbt-dagster/jaffle_shop/jaffle_dagster/jaffle_dagster/assets.py
@@ -1,9 +1,46 @@
-from dagster import AssetExecutionContext
+import os
+from pathlib import Path
+
+from dagster import AssetExecutionContext, op, job, Definitions, OpExecutionContext, ResourceDefinition
 from dagster_dbt import DbtCliResource, dbt_assets
 
 from .constants import dbt_manifest_path
 
-
+# Existing dbt assets definition
 @dbt_assets(manifest=dbt_manifest_path)
 def jaffle_shop_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
+
+@op(required_resource_keys={'dbt'})
+def run_dbt_build_with_stage_tag(context):
+    # Execute the dbt build command for models with the 'stage' tag
+    context.resources.dbt.cli(["build", "--select", "tag:stage"]).wait()
+
+dbt_project_dir = Path(__file__).resolve().parents[3].joinpath('jaffle_shop')
+
+@job(resource_defs={'dbt': ResourceDefinition.hardcoded_resource(DbtCliResource(project_dir=dbt_project_dir))})
+def run_dbt_build_stage_tag_job():
+    run_dbt_build_with_stage_tag()
+# # Get the absolute path to the dbt project directory
+# dbt_project_dir = Path(__file__).resolve().parents[3].joinpath('jaffle_shop')
+
+# dbt = DbtCliResource(project_dir=os.fspath(dbt_project_dir))
+
+# # New dbt operation for running models with tag 'stage'
+# @op
+# def run_dbt_stage_models(context: AssetExecutionContext, dbt: DbtCliResource):
+#     dbt.cli(["run", "--models", "tag:stage"]).wait()
+
+# @op
+# def run_dbt_tests(dbt: DbtCliResource):
+#     # Run dbt tests
+#     dbt.cli(["test"]).wait()
+
+# # Define jobs for each operation
+# @job(resource_defs={'dbt': DbtCliResource})
+# def run_stage_models_job():
+#     run_dbt_stage_models()
+
+# @job(resource_defs={'dbt': DbtCliResource})
+# def run_dbt_tests_job():
+#     run_dbt_tests()
--- tutorial-dbt-dagster/jaffle_shop/jaffle_dagster/jaffle_dagster/definitions.py
+++ tutorial-dbt-dagster/jaffle_shop/jaffle_dagster/jaffle_dagster/definitions.py
@@ -1,25 +1,16 @@
 import os
-
-from dagster import op, job, Definitions
+from dagster import Definitions
 from dagster_dbt import DbtCliResource
 
-from .assets import jaffle_shop_dbt_assets
+from .assets import jaffle_shop_dbt_assets,  run_dbt_build_stage_tag_job  #run_stage_models_job, run_dbt_tests_job,
 from .constants import dbt_project_dir
 from .schedules import schedules
 
 defs = Definitions(
     assets=[jaffle_shop_dbt_assets],
+    jobs=[run_dbt_build_stage_tag_job],  # Include the new jobs here run_stage_models_job, run_dbt_tests_job,
     schedules=schedules,
     resources={
         "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
     },
 )
-
-@op(required_resource_keys={'dbt'})
-def run_dbt_models_with_tag(context):
-    # Replace 'your_tag' with the specific tag you want to filter by
-    context.resources.dbt.run(models='tag:stage')
-
-@job(resource_defs={'dbt': DbtCliResource})
-def dbt_run_job():
-    run_dbt_models_with_tag()
